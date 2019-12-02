31e9cb10a2635541c1cd9007c6ef983e9a014787
Dalton Overmyer <dalton@raptorpowersystems.com>
2019-12-01 19:25:52 -0600

    init output for variables that are too large.

---
+++ scripts/output.js
@@ -0,0 +1,34 @@
+
+
+var introFile = "intro.txt";
+var introOutput =
+["  _____             _                                   \n",
+" |  __ \\           | |       /\\                         \n",
+" | |__) |___   ___ | |_     /  \\   ___ ___ ___  ___ ___ \n",
+" |  _  // _ \\ / _ \\| __|   / /\\ \\ / __/ __/ _ \\/ __/ __|\n",
+" | | \\ \\ (_) | (_) | |_   / ____ \\ (_| (_|  __/\\__ \\__ \\\n",
+" |_|  \\_\\___/ \\___/ \\__| /_/    \\_\\___\\___\\___||___/___/\n\n",
+"                           o",
+"                           o",
+"                          o o",
+"                         o   o",
+"                        o     o",
+"                       o o     o",
+"                      o   o   o o",
+"                   oo     o  o   o",
+"                  o   o     o     oo",
+"                 o     o    o     o o",
+"                o     o o   oo    o  o",
+"               o     o  o  o  o   o   oo",
+"               o    o   o o   o   o     o",
+"               o   o     o       o o     o",
+"              o    o    o o     o  o      o",
+"             o    o    o   o    o   o",
+"            o    o     o   o    o   o",
+"                  o        o    o    o",
+"                  o        o         o\n\n",
+"A Southern Illinois University Computer Science senior project by Dalton Overmyer, Christopher McNeil, and Thomas Mulugeta Abebe.\n",
+"Mentor: Dr. Geisler-Lee - Research Professor. Bioinformatics, Cell Biology, Cell Wall Biology, Metabolism at Southern Illinois University.\n\n",
+"Our main goal is to create a program that automatically crops the image of a root system. "];
+
+var resumes = ["dalton_resume", "thomas_cv", "christopher_resume"];
